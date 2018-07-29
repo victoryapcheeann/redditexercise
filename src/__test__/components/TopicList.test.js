b63cdf28ec520b908561fcc1b3ee7e4ef0031ff0
Victor Yap <victoryapcheeann@gmail.com>
2018-07-29 20:04:05 +0800

    added test for topiclist

--- src/__test__/components/TopicList.test.js
+++ src/__test__/components/TopicList.test.js
@@ -1,6 +1,23 @@
-/*
-TODO: 
-1) It has one text area and one button
-2) Has a textarea that users can type in 
-3) Form is cleared when submitted 
-*/
+import React from 'react';
+import { mount } from 'enzyme';
+import topics from '../../data/topics'
+import TopicsList from '../../TopicsList';
+import Root from '../../Root';
+
+let wrapped;
+
+beforeEach(() => {
+  const initialState = {
+    topics: topics
+  };
+
+  wrapped = mount(
+    <Root initialState={initialState}>
+      <TopicsList />
+    </Root>
+  );
+});
+
+it('creates one LI per topics, up to a maximum of 20 topics', () => {
+  expect(wrapped.find('li').length).toEqual(20);
+});
