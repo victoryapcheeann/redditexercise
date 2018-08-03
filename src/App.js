e304f5fc095297698a5023173f1b1b7e3a4466b9
Victor Yap <victoryapcheeann@gmail.com>
2018-08-03 12:04:13 +0800

    Added github link

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ class App extends Component{
     return (
       <div>
         <h2>Reddit Exercise</h2>
+        <a href="https://github.com/victoryapcheeann/redditexercise">Github Link</a>
+        <br />
+        <br />
         <TopicsCreateBox /> {/*This is the component to create new topics*/}
         <TopicsList /> {/*This is the component to view, upvote and downvote topics*/}
       </div>
